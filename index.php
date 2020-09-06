65918c4a04d16ad4ac0f09f17acc8fa0b711f064
filipelopes2020 <mfilipelopes@gmail.com>
2020-09-06 18:24:26 +0100

    Update index.php
    change1

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
   <title>PHP Test</title>
  </head>
  <body>
- <?php echo '<p>Hello World-Happy Learning. This is wonderful! This is my first Jenkins PHP200 page using OpenShift Online</p>'; ?>
+ <?php echo '<p>Hello World-Happy Learning. '; ?>
  </body>
 </html>
